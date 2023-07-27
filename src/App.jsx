9bbc348f196f1b18f19e91d75fc80cc1dab5faa2
Seogun95 <ksj083895@gmail.com>
2023-07-27 20:26:02 +0900

    feat: initial init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Test } from './pages/Test';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world</h1>
+      <Test />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+export const Test = () => {
+  return (
+    <div>
+      <Typography
+        variant="h2"
+        sx={{ color: 'tomato', fontWeight: 'bold', border: '1px solid' }}
+      >
+        gdgd
+      </Typography>
+    </div>
+  );
+};
